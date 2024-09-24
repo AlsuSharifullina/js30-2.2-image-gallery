4d523b74741de2fabe54521dbd4d67ccd00405ae
Alsu Sharifullina <sharifullina1998@inbox.ru>
2024-09-25 01:02:01 +0300

    init add img, header and styles

---
+++ image_gallery/script.js
@@ -0,0 +1,3 @@
+const apiUrl = "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo"
+
+const apiKey = "ykWPRzplJUs71JXAadgQwSjwajzcN8BQPrL2SPaXx7Q"
